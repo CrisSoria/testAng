7b047d506d785faee5c8bdbacb12bec276b01689
CrisSoria <crissoria07@gmail.com>
2021-11-14 01:02:18 -0300

    projects page terminada

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from 'src/app/services/projects.service';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
+  projectsShow: any = [];
+  projects: any = [];
+  icon: any = faSearch;
+  searchInput: string = '';
 
-  constructor() { }
+  constructor(private projectsService: ProjectsService) {}
 
   ngOnInit(): void {
+    this.projects = this.projectsService.projects;
+    this.projectsShow = this.projects;
   }
-
+  searchProject(e: Event): void {
+    this.projectsShow = this.projects.filter((p: any) =>
+      p.name.toLowerCase().match(this.searchInput.toLowerCase())
+    );
+  }
 }
